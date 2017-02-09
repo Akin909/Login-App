41b0ad11aae3ad789f08b6665c17f00ca5db56fc
Akin909 <akin.sowemimo@gmail.com>
2017-02-09 01:02:58 +0000

    add controller(s), user model schema and server as well as html and css for login  page

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require ('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require ('mongoose');
+var path = require ('path');
+var controller = require('./controllers/user.controller')
+
+var port = 8080;
+var db = 'mongodb://localhost:userlogin';
+
+ mongoose.connect(db);
+
+app.use(express.static(path.join(__dirname,'views')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended:true
+}));
+
+
+app.use('/',function(req,res) {
+	res.render('index.html');	
+});
+
+app.post('/',controller.register);
+
+
+app.listen(port,function() {
+	console.log('app listening on port ' + port);	
+});
+
+
+
